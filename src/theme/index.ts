e8422052bce32efcfb10968c9fcb5e701d9b9fbb
Victor Ferrari <victorfonsecaferrari@gmail.com>
2023-08-25 21:20:38 -0300

    FEAT: Theme Create

---
+++ src/theme/index.ts
@@ -0,0 +1,24 @@
+export default {
+    COLORS : {
+        WHITE : '#FFFFFF',
+        BACKGROUND : '#121214',
+        SHAPE_PRINCIPAL : '#202024',
+        SHAPE_SECUNDARIA : '#29292E',
+        SHAPE_TERCIARIA : '#323238',
+        PLACEHOLDER : '#7C7C8A',
+        TEXTO_BASE : '#C4C4C6',
+        TITULOS : '#E1E1E6'
+    },
+    FONT_FAMILY : {
+        REGULAR: 'Roboto-Regular',
+        BOLD: 'Roboto-Bold',
+        MEDIUM: 'Roboto-Medium',
+        LIGHT: 'Roboto-Light'
+    },
+    FONT_SIZE: {
+        SM: 14,
+        MD: 16,
+        LG: 18,
+        XL: 24
+    }
+}
